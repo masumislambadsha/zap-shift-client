1ecf23f34afc0128399bc398dcb7168f85e9841c
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-16 21:51:05 +0600

    updated reviews section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,7 +3,9 @@ import HowItWorks from "../HowItWorks/HowItWorks";
 import Banner from "../Banner/Banner";
 import OurServices from "../OurServices/OurServices";
 import Brands from "../Brands/Brands";
+import Reviews from "../Reviews/Reviews";
 
+const reviewsPromise = fetch("/reviews.json").then(res=>res.json())
 const Home = () => {
   return (
     <div>
@@ -11,6 +13,7 @@ const Home = () => {
       <HowItWorks />
       <OurServices/>
       <Brands/>
+      <Reviews reviewsPromise={reviewsPromise} />
     </div>
   );
 };
--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { use } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const Reviews = () => {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
+const Reviews = ({ reviewsPromise }) => {
+  const reviews = use(reviewsPromise);
+  console.log(reviews);
+
   return (
     <div>
-
+      <h3 className="text-3xl text-center">Reviews</h3>
+      <p className="text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error
+        voluptatem provident quos! Totam nam illo pariatur nemo reprehenderit!
+        Facilis, nulla molestias. Sint nesciunt necessitatibus nisi! Quaerat
+        repellendus repudiandae velit fugiat iusto! Voluptatum quae eligendi,
+        animi debitis corrupti ipsam explicabo dolore cumque nihil voluptatibus
+        iusto iure, a facere ratione eius odio?
+      </p>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
