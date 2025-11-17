ab8a9a320e39d4dc0c504ded171735bc0519dab7
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-17 22:21:06 +0600

    addded gotTo func in map

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useLoaderData } from "react-router";
@@ -6,18 +6,55 @@ import { useLoaderData } from "react-router";
 const Coverage = () => {
   const position = [23.8041, 90.4152];
   const serviceCenters = useLoaderData();
+  const mapRef = useRef(null)
   console.log(serviceCenters);
+
+  const handleSearch = e  =>{
+    e.preventDefault()
+    const location = e.target.location.value
+    const district = serviceCenters.find(c => c.district.toLowerCase().includes(location.toLowerCase()))
+    if(district) {
+      const coord = [district.latitude,district.longitude ]
+      console.log(district, coord);
+      mapRef.current.flyTo(coord,10)
+
+    }
+  }
 
   return (
     <div>
       <h1 className="text-4xl capitalize p-5 text-secondary font-bold">
         We are available in 64 districts
       </h1>
-      <div></div>
+      <div>
+        <form onSubmit={handleSearch}>
+          <label className="input">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input type="search" className="grow" placeholder="Search"
+          name="location" />
+        </label>
+        </form>
+      </div>
       <div className="w-full h-100 mx-auto">
         <MapContainer
           center={position}
           zoom={7}
+          ref={mapRef}
           className="w-full h-160  mx-auto"
         >
           <TileLayer
