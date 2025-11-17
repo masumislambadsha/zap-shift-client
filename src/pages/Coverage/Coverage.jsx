fe7ae9d420c8916ec33b6b97920bcb2b5c5c9f49
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-17 18:09:41 +0600

    addded place name and marker in map

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -5,32 +5,35 @@ import { useLoaderData } from "react-router";
 
 const Coverage = () => {
   const position = [23.8041, 90.4152];
-  const serviceCenters = useLoaderData()
+  const serviceCenters = useLoaderData();
   console.log(serviceCenters);
-  
+
   return (
     <div>
       <h1 className="text-4xl capitalize p-5 text-secondary font-bold">
         We are available in 64 districts
       </h1>
+      <div></div>
       <div className="w-full h-100 mx-auto">
         <MapContainer
           center={position}
-          zoom={13}
-          className="w-1/2 h-100  mx-auto"
+          zoom={7}
+          className="w-full h-160  mx-auto"
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          {serviceCenters.map((center, ind) => (
+            <Marker key={ind} position={[center.latitude, center.longitude]}>
+              <Popup>
+                <strong> {center.district}.</strong> <br />{" "}
+                {center.covered_area.join(", ")}
+              </Popup>
+            </Marker>
+          ))}
         </MapContainer>
       </div>
-      <div></div>
     </div>
   );
 };
