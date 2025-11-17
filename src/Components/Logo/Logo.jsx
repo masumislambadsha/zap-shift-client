ea58a0855d74e0b7e5b1c06c02ba0e688580fcc3
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-16 18:27:15 -0800

    added custom theme, customized footer and navbar

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import logo from "../../assets/logo.png"
 const Logo = () => {
   return (
-    <div>
-
+    <div className='flex items-end'>
+       <img src={logo} alt="" />
+       <h3 className="text-3xl font-bold -ml-2">zapShift</h3>
     </div>
   );
 };
