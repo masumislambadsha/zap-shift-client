7080159d0d803e1469dd0d72eef5b8a752bd3674
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-18 08:41:17 +0600

    update change

--- src/Context/AuthProvider/AuthProvider.jsx
+++ src/Context/AuthProvider/AuthProvider.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { AuthContext } from '../AuthContext/AuthContext';
+import React from "react";
+import { AuthContext } from "../AuthContext/AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../Firebase/firebase.init";
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  const registerUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-  const authInfo ={
-
-  }
-
-
-  return (
-    <AuthContext value={""}>
-        {children}
-    </AuthContext>
-  );
+  const authInfo = {
+    registerUser,
+    signInUser,
+  };
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
