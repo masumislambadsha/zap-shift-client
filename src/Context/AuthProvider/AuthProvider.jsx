89e2d74d339bd183f7c0f01a776b34ee4160c6c2
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-18 09:42:16 +0600

    update change

--- src/Context/AuthProvider/AuthProvider.jsx
+++ src/Context/AuthProvider/AuthProvider.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "../AuthContext/AuthContext";
 import {
   createUserWithEmailAndPassword,
   GoogleAuthProvider,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signInWithPopup,
 } from "firebase/auth";
@@ -11,17 +12,33 @@ import { auth } from "../../Firebase/firebase.init";
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const registerUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const signInUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
   const signInGoogle = () => {
+    setLoading(true);
     signInWithPopup(auth, googleProvider);
   };
 
+  useEffect(() =>{
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) =>{
+      setUser(currentUser)
+    })
+    return () =>{
+      unsubscribe()
+    }
+  },[])
+
   const authInfo = {
+    user,
+    loading,
     registerUser,
     signInUser,
     signInGoogle,
