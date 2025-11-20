ddfd00bb2aea142f9e912d858fbd182871213103
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-20 15:13:28 +0600

    update change

--- src/pages/SendAParcel/SendAParcel.jsx
+++ src/pages/SendAParcel/SendAParcel.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const SendAParcel = () => {
+  const {} = useForm()
   return (
     <div>
-
+      <h2 className="text-5xl font-bold">Send A Parcel</h2>
+      <form>
+        {/* document */}
+        <div></div>
+        {/* parcel info */}
+        <div></div>
+        {/* two column */}
+        <div>
+          {/* sender */}
+          <div></div>
+          {/* reciver */}
+          <div></div>
+        </div>
+      </form>
     </div>
   );
 };
