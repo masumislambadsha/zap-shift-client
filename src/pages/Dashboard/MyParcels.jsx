683663d1b3b441422f4e360879663d4cff1dc822
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-22 23:47:00 +0600

    update change

--- src/pages/Dashboard/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels.jsx
@@ -16,6 +16,31 @@ const MyParcels = () => {
   return (
     <div>
       My All Parcels : {parcels.length}
+      <div className="overflow-x-auto">
+  <table className="table table-zebra">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Cost</th>
+        <th>Payment Status</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        parcels.map((parcel,ind) =><tr key={parcel._id}>
+        <th>{ind + 1}</th>
+        <td>{parcel.parcelName}</td>
+        <td>{parcel.cost}</td>
+        <td>Blue</td>
+        <td>Delete</td>
+      </tr>)
+      }
+      </tbody>
+  </table>
+</div>
     </div>
   );
 };
