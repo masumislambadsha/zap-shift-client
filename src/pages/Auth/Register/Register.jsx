b320e17df622a59790a7e7b1d16dc0b6992f63f2
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-24 23:22:01 +0600

    update change

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -19,7 +19,7 @@ const Register = () => {
   const [loading, setLoading] = useState(false);
 
   const handleRegistration = (data) => {
-    setLoading(true); // Start loading
+    setLoading(true);
 
     const profileImg = data.photo[0];
     registerUser(data.email, data.password)
@@ -27,42 +27,48 @@ const Register = () => {
         const formData = new FormData();
         formData.append("image", profileImg);
 
-        const image_Api_Url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_host_key}`;
-        axios.post(image_Api_Url, formData).then((res) => {
-          const photoURL = res.data.data.url;
-          const userInfo = {
-            email: data.email,
-            displayName: data.name,
-            photoURL: photoURL,
-          };
+        const image_Api_Url = `https://api.imgbb.com/1/upload?key=${
+          import.meta.env.VITE_image_host_key
+        }`;
+        axios
+          .post(image_Api_Url, formData)
+          .then((res) => {
+            const photoURL = res.data.data.url;
+            const userInfo = {
+              email: data.email,
+              displayName: data.name,
+              photoURL: photoURL,
+            };
 
-          axiosSecure.post('/users', userInfo)
-            .then(res => {
-              setLoading(false); // End loading
-              if (res.data.insertedId) {
-                toast.success("Registration Successful");
-                 navigate(location?.state || "/");
-              }
-            })
-            .catch(err => {
-              setLoading(false);
-              toast.error("User registration failed!");
-              console.log(err);
-            });
+            axiosSecure
+              .post("/users", userInfo)
+              .then((res) => {
+                setLoading(false); // End loading
+                if (res.data.insertedId) {
+                  toast.success("Registration Successful");
+                  navigate(location?.state || "/");
+                }
+              })
+              .catch((err) => {
+                setLoading(false);
+                toast.error("User registration failed!");
+                console.log(err);
+              });
 
-          const userProfile = {
-            displayName: data.name,
-            photoURL: photoURL,
-          };
-          updateUserProfile(userProfile)
-            .then(() => console.log("user profile updated"))
-            .catch((err) => {
-              console.log(err);
-            });
-        }).catch(err => {
-          setLoading(false);
-          console.log(err);
-        });
+            const userProfile = {
+              displayName: data.name,
+              photoURL: photoURL,
+            };
+            updateUserProfile(userProfile)
+              .then(() => console.log("user profile updated"))
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            setLoading(false);
+            console.log(err);
+          });
       })
       .catch((err) => {
         setLoading(false);
@@ -74,6 +80,18 @@ const Register = () => {
     signInGoogle()
       .then((res) => {
         console.log(res.user);
+        const userInfo = {
+          email: res.user.email,
+          displayName: res.user.displayName,
+          photoURL: res.user.photoURL,
+        };
+        axiosSecure.post("/users", userInfo).then((res) => {
+          if (res.data.insertedId) {
+            console.log("User data has been stored in DB", res.data);
+            navigate(location?.state || "/");
+            toast.success("Registered With Google Successfully");
+          }
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -160,33 +178,33 @@ const Register = () => {
         onClick={handleGoogleSignIn}
         className="btn bg-white text-black border-[#e5e5e5] mb-6"
       >
- <svg
-            aria-label="Google logo"
-            width="16"
-            height="16"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <g>
-              <path d="m0 0H512V512H0" fill="#fff"></path>
-              <path
-                fill="#34a853"
-                d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-              ></path>
-              <path
-                fill="#4285f4"
-                d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-              ></path>
-              <path
-                fill="#fbbc02"
-                d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-              ></path>
-              <path
-                fill="#ea4335"
-                d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-              ></path>
-            </g>
-          </svg>
+        <svg
+          aria-label="Google logo"
+          width="16"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <g>
+            <path d="m0 0H512V512H0" fill="#fff"></path>
+            <path
+              fill="#34a853"
+              d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+            ></path>
+            <path
+              fill="#4285f4"
+              d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+            ></path>
+            <path
+              fill="#fbbc02"
+              d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+            ></path>
+            <path
+              fill="#ea4335"
+              d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+            ></path>
+          </g>
+        </svg>
         Login with Google
       </button>
       {/* Loader CSS below */}
