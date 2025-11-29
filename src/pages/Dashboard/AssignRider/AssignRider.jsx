dd76455c0d659ff907073310675272cf68218068
masumislambadsha <nkfhhdndjdh@gmail.com>
2025-11-30 01:54:40 +0600

    updatee

--- src/pages/Dashboard/AssignRider/AssignRider.jsx
+++ src/pages/Dashboard/AssignRider/AssignRider.jsx
@@ -1,9 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 
 const AssignRider = () => {
+  const axiosSecure = useAxiosSecure()
+  const {data : parcels = []} = useQuery({
+    queryKey : ['parcel', "pending-pickup"],
+    queryFn: async () =>{
+      const res = await axiosSecure.get("")
+      return res.data
+    }
+  })
   return (
     <div>
-
+      <h2 className="text-5xl">Assign Rider {parcels.length} </h2>
     </div>
   );
 };
